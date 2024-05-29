ba4dbf27aa9e4eb845b0b850159c377542d3e8d4
viviciu <vstaicu@andrew.cmu.edu>
2024-05-28 23:38:16 -0400

    reformatted date to ::MM-DD-YYYY

--- portfolio/app/blogs/page.tsx
+++ portfolio/app/blogs/page.tsx
@@ -6,8 +6,21 @@ import Navbar from "../_components/Navbar"
 
 export default async function BlogsPage() {
   const blogs = await getBlogs()
-  // sort by date
-  blogs.sort((a, b) => new Date(b.frontmatter.publishDate) - new Date(a.frontmatter.publishDate))
+  // Sort by date
+
+  // The Date() constructor in JavaScript can parse a variety of date string formats. It's generally recommended to use a format that conforms to the ISO 8601 standard, which is YYYY-MM-DD for just the date. -- Github Copilot
+
+  // I have an idea. I could make the date string, use string slicing to separate it, assign each slice to a variable, then reconstruct it in YYY-MM-DD format. Then I could use the Date() constructor to parse it.
+  // blogs.sort((a, b) => new Date(b.frontmatter.publishDate).getTime() - new Date(a.frontmatter.publishDate).getTime())
+
+  blogs.sort((a, b) => {
+    const aDateParts = a.frontmatter.publishDate.split('-');
+    const bDateParts = b.frontmatter.publishDate.split('-');
+    const aDate = new Date(`${aDateParts[2]}-${aDateParts[0]}-${aDateParts[1]}`);
+    const bDate = new Date(`${bDateParts[2]}-${bDateParts[0]}-${bDateParts[1]}`);
+    return bDate.getTime() - aDate.getTime();
+  });
+
   return (
     <main>
       <Navbar />
@@ -28,7 +41,7 @@ export default async function BlogsPage() {
             </Link>
             
             <p>{blog.frontmatter.author}</p>
-            <p>{blog.frontmatter.publishDate}</p>
+            <p>::{blog.frontmatter.publishDate}</p>
             {/* <Link href={`/blogs/${blog.slug}`}>Read More</Link> */}
           </article>
         
