b2adf7be6c2a6a6fbfe1eefd44d5303c1a79d32d
viviciu <vstaicu@andrew.cmu.edu>
2024-06-02 13:32:48 -0400

    messed with the head element in layout.js

--- portfolio/app/page.jsx
+++ portfolio/app/page.jsx
@@ -1,9 +1,16 @@
 import Navbar from './_components/Navbar.jsx';
 import Footer from './_components/Footer';
+import Head from 'next/head.js';
+import { metadata } from './layout.js';
+
 
 export default function Home() {
   return (
     <main>
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="portfolio/public/assets/images/apple.svg" />
+        <title>{metadata.title}</title>
+      </Head>
       <Navbar />
       <Footer />
     </main>
