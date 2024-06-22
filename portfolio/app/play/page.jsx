e9cdd1615cd417c6dde99c1b05b9b90bdf1f1d56
viviciu <vstaicu@andrew.cmu.edu>
2024-06-22 09:51:23 -0400

    remvd play experiment (failed html horizontal scroll)

--- portfolio/app/play/page.jsx
+++ portfolio/app/play/page.jsx
@@ -1,26 +1,12 @@
 import Navbar from "../_components/Navbar"
-// import "./app.js
-// import "./play.css"
-import CreateProjects from "./projects.jsx";
+
 
 export default function Play() {
     return (
         <main>
             <Navbar />
             <h2>Play</h2>
-            <section id="projects">
-                <div className="projects__sticky">
-                    <div className="slider__container">
-                        <div className="projects__slider">
-                        {/* <CreateProjects /> */}
-                        </div>
-                    </div>
-                </div>
-            </section>
-
             
-
-
         </main>
     
     )
