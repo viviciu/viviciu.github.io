a19787668f28621a1435d9f24a1df4f366a66eb4
viviciu <vstaicu@andrew.cmu.edu>
2024-06-24 08:10:07 -0400

    updated play with grid like sameOldStudios

--- portfolio/app/_components/Footer.jsx
+++ portfolio/app/_components/Footer.jsx
@@ -5,7 +5,8 @@ const Footer = () => {
     <main>
       <h1 className="uppercase lg:text-[36rem] text-[6rem] tracking-tighter font-bold m-0 max-h-[45rem]">staicu</h1>
       <div className="grid lg:grid-cols-5 grid-cols-3 lg:px-0 px-8 pb-2 gap-4 w-full">
-        <p className="col-start-1">© 2021 staicu. All rights reserved.</p>
+        {/* use `shift+option+dash` to insert long dash character `—` */}
+        <p className="col-start-1">© 2024 STAICU — Design+HCI @ Carnegie Mellon</p>
         {/* LEFT OFF HERE WORKING ON ALIGNING GRID IN FOOTER. NEXT STEPS: FIX THE HORIZONTAL SCROLL! */}
         <p className='lg:col-start-3 justify-self-end self-end'>a.</p>
         <p className='lg:col-start-4 col-start-3'>Margaret Morrison Carnegie Hall, <br /> Pittsburgh, PA 15213</p>
