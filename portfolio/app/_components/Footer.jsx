bec0953de22dc274d8fbf5c251422e17248b9ad8
viviciu <vstaicu@andrew.cmu.edu>
2024-08-13 10:31:20 -0400

    footer archive link broken

--- portfolio/app/_components/Footer.jsx
+++ portfolio/app/_components/Footer.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <main>
-      <h1 className="uppercase lg:text-[30vw] text-[30vw] tracking-tighter font-bold m-0 max-h-[45rem]">staicu</h1>
+    <main className="m-4">
+      <h2 className="text-5xl text-display ">
+        see the <Link className='underline' href="/blog">archive</Link>—
+      </h2>
+      <h1 className="inline-block uppercase lg:text-[30vw] text-[30vw] tracking-tighter font-bold m-0 leading-none max-h-[45rem]">
+        staicu
+      </h1>
       {/* Feldorean */}
       <div className="grid lg:grid-cols-5 grid-cols-3 lg:px-0 px-8 pb-2 gap-4 w-full">
         <p className="col-start-1">© 2021 staicu. All rights reserved.</p>
         {/* LEFT OFF HERE WORKING ON ALIGNING GRID IN FOOTER. NEXT STEPS: FIX THE HORIZONTAL SCROLL! */}
-        <p className='lg:col-start-3 justify-self-end self-end'>a.</p>
-        <p className='lg:col-start-4 col-start-3'>Margaret Morrison Carnegie Hall, <br /> Pittsburgh, PA 15213</p>
+        <p className="lg:col-start-3 justify-self-end self-end">a.</p>
+        <p className="lg:col-start-4 col-start-3">
+          Margaret Morrison Carnegie Hall, <br /> Pittsburgh, PA 15213
+        </p>
       </div>
     </main>
-  )
+  );
 }
 
 export default Footer
--- portfolio/app/play/layout.jsx
+++ portfolio/app/play/layout.jsx
@@ -4,7 +4,6 @@ export default function PlayLayout({ children }) {
   // Create any shared layout or styles here
   return (
     <section>
-      {/* <Navbar /> */}
       {children}
       <Footer />
     </section>
