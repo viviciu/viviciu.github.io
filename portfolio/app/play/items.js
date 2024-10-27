73094d2d482bbd9ece5573282c22a33bf27e6e9a
viviciu <vstaicu@andrew.cmu.edu>
2024-10-26 21:50:54 -0400

    added more to ~/play, felt sad looking at old highschool videos.

--- portfolio/app/play/items.js
+++ portfolio/app/play/items.js
@@ -1,61 +1,10 @@
 // A separate file with items (url should contain a link for each image)
 export const items = [
   {
-    id: "scifirender", // id should be the title of the project folder, stored under ./play
-    name: "",
-    url: "/imgs/play/sci-fi-render-01.jpg",
-    category: "3d",
-    categorySpecific:
-      "",
-  },
-  {
-    id: "backpack",
-    name: "backpack",
-    url: "/IMAG0009.JPG",
-    category: "sketch",
-    categorySpecific: "",
-  },
-  {
-    id: "item-03",
-    name: "Item 3",
-    url: "/refrigerator-lut_1.2.1.jpg",
-    category: "video", // this must be changed if category changes in html
-    categorySpecific: "",
-  },
-  {
-    id: "item-04",
-    name: "Item 3",
-    url: "/lucy-2-prada.jpg",
-    category: "photo",
-    categorySpecific: "",
-  },
-  {
-    id: "05",
-    name: "Item 3",
-    url: "/ss-plane-slog.png",
-    category: "video",
-    categorySpecific: "",
-  },
-  {
-    id: "06", // id should be the title of the project folder, stored under ./play
-    name: "envScene",
-    url: "/imgs/play/envSceneRender.jpg",
-    category: "3d",
-    categorySpecific:
-      "",
-  },
-  {
-    id: "07",
-    name: "vhs",
-    url: "/imgs/play/vhs-gif.gif",
-    category: "3d",
-    categorySpecific: "",
-  },
-  {
-    id: "08",
-    name: "xr-01",
-    url: "/imgs/play/ceiling-blockout03.png",
-    category: "3d", 
+    id: "pminiProjII",
+    name: "pminiProjII",
+    url: "/imgs/play/pminiProjII.gif",
+    category: "product",
     categorySpecific: "",
   },
   {
@@ -80,13 +29,65 @@ export const items = [
     category: "product",
     categorySpecific: "",
   },
+
   {
-    id: "12",
-    name: "Item 3",
-    url: "/imgs/play/DSC05648.JPG",
-    category: "product",
+    id: "scifirender", // id should be the title of the project folder, stored under ./play
+    name: "",
+    url: "/imgs/play/sci-fi-render-01.jpg",
+    category: "3d",
     categorySpecific: "",
   },
+
+  {
+    id: "item-03",
+    name: "Item 3",
+    url: "/refrigerator-lut_1.2.1.jpg",
+    category: "video", // this must be changed if category changes in html
+    categorySpecific: "",
+  },
+  {
+    id: "item-04",
+    name: "Item 3",
+    url: "/lucy-2-prada.jpg",
+    category: "photo",
+    categorySpecific: "",
+  },
+  {
+    id: "05",
+    name: "Item 3",
+    url: "/ss-plane-slog.png",
+    category: "video",
+    categorySpecific: "",
+  },
+  {
+    id: "05",
+    name: "Item 3",
+    url: "/imgs/play/wipedClean_nonOptimized.gif",
+    category: "video",
+    categorySpecific: "",
+  },
+  {
+    id: "06", // id should be the title of the project folder, stored under ./play
+    name: "envScene",
+    url: "/imgs/play/envSceneRender.jpg",
+    category: "3d",
+    categorySpecific: "",
+  },
+  {
+    id: "07",
+    name: "vhs",
+    url: "/imgs/play/vhs-gif.gif",
+    category: "3d",
+    categorySpecific: "",
+  },
+  {
+    id: "08",
+    name: "xr-01",
+    url: "/imgs/play/ceiling-blockout03.png",
+    category: "3d",
+    categorySpecific: "",
+  },
+
   {
     id: "13",
     name: "Item 3",
@@ -102,9 +103,23 @@ export const items = [
     categorySpecific: "",
   },
   {
+    id: "14",
+    name: "Item 3",
+    url: "/imgs/play/headEmptyCharcoal.jpg",
+    category: "sketch",
+    categorySpecific: "",
+  },
+  {
     id: "15",
     name: "Item 3",
     url: "/imgs/play/thumb4_161-crop.jpg",
+    category: "sketch",
+    categorySpecific: "",
+  },
+  {
+    id: "backpack",
+    name: "backpack",
+    url: "/IMAG0009.JPG",
     category: "sketch",
     categorySpecific: "",
   },
