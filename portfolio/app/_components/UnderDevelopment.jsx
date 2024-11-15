699d8af7da9d7c7b8dbbbebfe453fa67c623d436
viviciu <vstaicu@andrew.cmu.edu>
2024-11-14 21:35:28 -0500

    added photos to play, removed old liminal content

--- portfolio/app/_components/UnderDevelopment.jsx
+++ portfolio/app/_components/UnderDevelopment.jsx
@@ -1,9 +1,19 @@
-export default function UnderDevelopment() {
-    return (
-        <main>
-            <h2 className="h-screen flex justify-center items-center">under development</h2>
-            
-        </main>
-    
-    )
+import PropTypes from "prop-types";
+
+export default function UnderDevelopment({ text }) {
+  return (
+    <main>
+      <h2 className="h-screen flex justify-center items-center">{text}</h2>
+    </main>
+  );
 }
+
+// Set default props
+UnderDevelopment.defaultProps = {
+  text: "under development",
+};
+
+// Define prop types
+UnderDevelopment.propTypes = {
+  text: PropTypes.string,
+};
