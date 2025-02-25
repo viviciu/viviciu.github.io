9da30453831ace5eef41f839ebacfa15597f7d7a
viviciu <vstaicu@andrew.cmu.edu>
2025-02-24 21:36:56 -0500

    done

--- my-app/app/work/elysium/page.jsx
+++ my-app/app/work/elysium/page.jsx
@@ -23,28 +23,26 @@ import New2 from "../../_components/New2";
 import New3 from "../../_components/New3";
 import Image from "next/image";
 import UnderDevelopment from "../../_components/UnderDevelopment"
-
 import "../../_components/greyBGLines.css";
 import Footer from "../../_components/Footer";
 import Head from "next/head";
+import New3VideoCloudinaryAutoplayLoop from "@/app/_components/New3VideoCloudinaryAutoplayLoop";
+
+import { MdOutlineArrowOutward } from "react-icons/md";
+import Link from "next/link";
 
 export default function Liminal() {
   const members = [
-    "Yon Maor",
-    "Stacy Chen",
-    "Chris Song",
-    "Ayden Xu",
-    "Ervin Song",
-    "Eric Lin",
-    "Minsung Kang",
+    "Suyeon Cha",
+    "Suanna Zhong",
+    "Margaret He",
+    "Xiao Yuan",
+    "Serena Cai",
+    "Kaitlyn Ng",
+    "Lucy Wang",
   ];
-  const tools = [
-    "Three.js",
-    "React Three Fiber",
-    "Figma",
-    "Blender"
-  ]
-  const myRole = "Creative Technologist";
+  const tools = ["Three.js", "GLSL", "Touchdesigner", "UX/UI", "Figma"];
+  const myRole = "Co-head, Creative Technologist";
   const timeline = "6 months"
 
 
@@ -52,7 +50,136 @@ export default function Liminal() {
   // A realm where the very essence of self and time finds itself in flux.
   return (
     <>
-    <UnderDevelopment />
+      <main className=" block px-4">
+        <Title
+          text1={
+            <div>
+              ELYSIUM, <br /> Paradise is not a place, it is the act of
+              becoming.
+            </div>
+          }
+        />
+
+        {/* Role: Interaction Design, 3D Web Development, UI/UX Immersive Experiences" ? */}
+        <YAMLBar
+          client="Lunar Gala"
+          year="2025"
+          role="Co-head of Web, Creative Technologist"
+        />
+        {/* COVER YAML EXPANDED */}
+        <main className=" pb-[5.06rem]">
+          <div className="h-fit w-full">
+            <video className="w-full h-auto" autoPlay loop muted playsInline>
+              <source
+                src={
+                  "https://res.cloudinary.com/ddjxpkewq/video/upload/f_auto:video,q_auto/v1/elysium/lx2pjp7hndviaxq2pjwv"
+                }
+                type="video/mp4"
+              />
+            </video>
+          </div>
+        </main>
+
+        <Context2
+          context={
+            <div>
+              <Link
+                href="https://2025.lunargala.org"
+                className="relative group underline"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Visit site
+                <MdOutlineArrowOutward
+                  className="inline-block ml-1 opacity-0 group-hover:opacity-100 transition-opacity duration-00 leading-p3
+        lg:text-p
+        text
+        font-SuisseWorks
+        font-regular
+    "
+                />
+              </Link>
+              <br /> <br />
+              Lunar Gala (LG) is Pittsburgh&apos;s largest annual fashion show.
+              It&apos;s website features the model lines, members, and ticketing
+              all in line with this year&apos;s visual identity: <i>Elysium</i>
+              .
+              <br /> <br /> I creatively directed and facilitated
+              cross-functional collaboration in a team of 4 designers and 5
+              devs. Lead concept design, interaction, and UI / UX for
+              Pittsburgh&apos;s largest fashion show&apos;s website.
+              <br /> <br />
+              I designed and developed the site&apos;s home page
+              GLSL particles to create a library of different particle behaviors
+              to fit with this year&apos;s identity, and the UI motion.
+              {/* TODO include blurb about archive */}
+            </div>
+          }
+          myRole={myRole}
+          members={members}
+          tools={tools}
+          timeline={timeline}
+        />
+
+        <DividerLine text={"HOME: GLSL Particle system"} />
+
+        <Header text={"How can particle motion encapsulate each act?"} />
+        <New3VideoCloudinaryAutoplayLoop
+          src={
+            "https://res.cloudinary.com/ddjxpkewq/video/upload/f_auto:video,q_auto/v1/elysium/b3wfxabgpgpm7z9gsw6w"
+            // https://res.cloudinary.com/ddjxpkewq/video/upload/f_auto:video,q_auto/v1/elysium/ygpilt8rzxvf4kqj3nyp
+            //
+          }
+          caption={""}
+          text1={
+            <div>
+              Act I: Emergence <br /> <br />
+              The dawn of your journey, brimming with hope, passion, and light.
+              Untouched by the world’s challenges, yet poised to embrace it all.
+            </div>
+          }
+        />
+        <New3VideoCloudinaryAutoplayLoop
+          src={
+            "https://res.cloudinary.com/ddjxpkewq/video/upload/f_auto:video,q_auto/v1/elysium/peypqarqejx6fezrlawh"
+          }
+          caption={""}
+          text1={
+            <div>
+              Act II: Blossom <br /> <br />
+              In the early steps of your journey, you flourish and grow. You
+              conquer the new horizons, and you can see yourself blooming into
+              the person you have always wanted to be.
+            </div>
+          }
+        />
+        <New3VideoCloudinaryAutoplayLoop
+          src={
+            "https://res.cloudinary.com/ddjxpkewq/video/upload/f_auto:video,q_auto/v1/elysium/d05qjnyo3ahj9quokazr"
+            // https://res.cloudinary.com/ddjxpkewq/video/upload/f_auto:video,q_auto/v1/elysium/ygpilt8rzxvf4kqj3nyp
+            //
+          }
+          caption={""}
+          text1={
+            <div>
+              Act IV: Embrace <br /> <br />
+              You have walked far, encountering all the pitfalls and flaws of
+              the world, therefore exposing the flaws of you. You have come to
+              realize there is never an end to your journey, but you have come
+              to embrace it.
+            </div>
+          }
+        />
+
+        <Header text={"Preloader"} />
+        <New3VideoCloudinaryAutoplayLoop
+          src={
+            "https://res.cloudinary.com/ddjxpkewq/video/upload/f_auto:video,q_auto/v1/lg/uelohmn4ldqfkuldngoo"
+          }
+          caption={""}
+          text1={""}
+        />
+      </main>
     </>
   );
 }
